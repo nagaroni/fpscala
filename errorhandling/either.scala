97459789cd2c83db1ed317e7a8e800ba1a3787d8
Gabriel Lima <gabriel.wprog@gmail.com>
2020-06-04 16:32:50 -0300

    Add either example

--- errorhandling/either.scala
+++ errorhandling/either.scala
@@ -29,6 +29,25 @@ def sequence[E, A](es: List[Either[E, A]]) : Either[E, List[A]] = es match {
 }
 
 def traverse[E, A, B](as: List[A])(f: A => Either[E, B]) : Either[E, List[B]] = as match {
-  case head :: tail => f(head).flatMap(hh => traverse(tail).map(tt => hh :: tt))
+  case head :: tail => f(head).flatMap(hh => traverse(tail)(f).map(tt => hh :: tt))
   case _ => Right(List[B]())
 }
+
+def sequence2[E, A](es: List[Either[E, A]]) : Either[E, List[A]] =
+  traverse(es)(x => x.map(v => v))
+
+
+case class Person(name: Name, age: Age)
+sealed class Name(val value: String)
+sealed class Age(val value: Int)
+
+def mkName(name: String) : Either[String, Name] =
+  if(name == "" || name == null) Left("Name is empty.")
+  else Right(new Name(name))
+
+def mkAge(age: Int) : Either[String, Age] =
+  if (age < 0) Left("Age is out of range.")
+  else Right(new Age(age))
+
+def mkPerson(name: String, age: Int) : Either[String, Person] =
+  mkName(name).map2(mkAge(age))(Person(_, _))
