75ae0a59a8422d55eca8f8bd3d9c3985586cb55d
Gabriel Lima <gabriel.wprog@gmail.com>
2020-08-19 22:29:32 -0300

    Add state transitions actions

--- purelyfunctionalstate/simplerng.scala
+++ purelyfunctionalstate/simplerng.scala
@@ -1,4 +1,7 @@
-package fpscala.purelyfunctionalstate.simplerng;
+package fpscala.purelyfunctionalstate.simplerng
+
+import java.util.Random
+;
 
 trait RNG {
   def nextInt: (Int, RNG)
@@ -7,6 +10,8 @@ trait RNG {
   def doubleInt(rng: RNG): ((Double, Int), RNG)
   def double3(rng: RNG): ((Double, Double, Double), RNG)
 }
+
+type Rand[+A] = RNG => (A, RNG)
 
 case class SimpleRNG(seed: Long) extends RNG {
   def nextInt: (Int, RNG) = {
@@ -59,5 +64,21 @@ case class SimpleRNG(seed: Long) extends RNG {
   }
 
   def ints(count: Int)(rng: RNG): (List[Int], RNG) = {
+    if (count > 0) {
+      val t = rng.nextInt
+      val r = ints(count - 1)(t._2)
+      (List(t._1) ::: r._1, r._2)
+    } else {
+      (List[Int](), rng)
+    }
   }
+
+  def map[A, B](s: Rand[A])(f: A => B): Rand[B] = rng => {
+    val (a, rng2) = s(rng)
+    (f(a), rng2)
+  }
+
+  def unit[A](a: A): Rand[A] = rng => (a, rng)
+
+  def nonNegativeEven: Rand[Int] = map(nonNegativeInt)(i => i - i % 2)
 }
