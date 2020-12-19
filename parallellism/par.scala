b2617aed91d762dbe1571d7516a0e5b9f38d87ee
Gabriel Lima <gabriel.wprog@gmail.com>
2020-12-19 01:08:07 -0300

    Add map2 to Par

--- parallellism/par.scala
+++ parallellism/par.scala
@@ -4,15 +4,16 @@ case class Par[+A](value: A)
 case object Par {
   def unit[A](value: => A): Par[A] = Par[A](value)
   def get[A](par: Par[A]): A = par.value
+  def map2[A, C](a: Par[A], b: Par[A])(f: (A, A) => C) : Par[C] = {
+    Par(f(a.value, b.value))
+  }
 }
 
-def sum(ints: IndexedSeq[Int]): Int = {
+def sum(ints: IndexedSeq[Int]): Par[Int] = {
   if (ints.size <= 1) {
-    ints.headOption.getOrElse(0)
+    Par.unit(ints.headOption.getOrElse(0))
   } else {
     val (l, r) = ints.splitAt(ints.length/2)
-    val sumL: Par[Int] = Par.unit(sum(l))
-    val sumR: Par[Int] = Par.unit(sum(r))
-    Par.get(sumL) + Par.get(sumR)
+    Par.map2(sum(l), sum(r))(_ + _)
   }
 }
