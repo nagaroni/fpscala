57c6126e499efdafa5752b6d982f47d408e859ff
Gabriel Lima <gabriel.wprog@gmail.com>
2021-02-23 21:48:20 -0300

    Change par

--- parallellism/par.scala
+++ parallellism/par.scala
@@ -1,13 +1,27 @@
-package parallellism.par
+package parallellism.par;
 
-case class Par[+A](value: A)
-case object Par {
-  def unit[A](value: A): Par[A] = Par[A](value)
-  def get[A](par: Par[A]): A = par.value
-  def fork[A](value: => Par[A]): Par[A] = value
-  def lazyUnit[A](value: => A) : Par[A] = fork(unit(a))
-  def map2[A, C](a: Par[A], b: Par[A])(f: (A, A) => C) : Par[C] = {
-    Par(f(a.value, b.value))
+import java.util.concurrent.{ExecutorService, TimeUnit, Callable, Future};
+
+type Par[A] = ExecutorService => Future[A]
+object Par {
+  def run[A](s: ExecutorService)(a: Par[A]) : Future[A] = a(s)
+  def unit[A](value: A): Par[A] = (es: ExecutorService) => UnitFuture(value)
+
+  private case class UnitFuture[A](get: A) extends Future[A] {
+    def isDone = true
+    def get(timeout: Long, units: TimeUnit) : A = get
+    def isCancelled = false
+    def cancel(evenIfRunning: Boolean) : Boolean = false
+  }
+  def fork[A](a: => Par[A]): Par[A] = es => es.submit(new Callable[A] {
+    def call = a(es).get
+  })
+  def lazyUnit[A](value: => A) : Par[A] = fork(unit(value))
+  // how to fix map2 to respect timeout from future?
+  def map2[A,B,C](a: Par[A], b: Par[B])(f: (A,B) => C) : Par[C] = (es: ExecutorService) => {
+    val af = a(es)
+    val bf = b(es)
+    UnitFuture(f(af.get, bf.get))
   }
 }
 
@@ -19,3 +33,7 @@ def sum(ints: IndexedSeq[Int]): Par[Int] = {
     Par.map2(Par.fork(sum(l)), Par.fork(sum(r)))(_ + _)
   }
 }
+
+// import java.util.concurrent.ForkJoinPool
+// val executor = new ForkJoinPool()
+// sum(IndexedSeq(1,2,3,4,5))(executor) => 15
